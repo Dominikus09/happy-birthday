cc4efe98b79abff8dafc6bffa0188b2f28e9d7a3
Dominikus09 <andikadk02@gmail.com>
2022-11-18 18:37:19 +0700

    amcklln

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  rewrites: async () => {
+    return [
+      {
+        source: "/public/index.htm",
+        destination: "/pages/api/myfile.js",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
